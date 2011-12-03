b391baeaf3e6f3641d5df1593a8ebcefcbc8e8ac
vishayv <vishay.vanjani@gmail.com>
2011-12-03 11:36:59 -0700

    updated stage 2 mapper reducers

--- charikar/mapper2.py
+++ charikar/mapper2.py
@@ -7,7 +7,6 @@ import sys
 
 # this mapper outut the file \t and its sketch 
 
-prev_file = "" 
 for line in sys.stdin:
 
 	line = line.strip()
@@ -22,56 +21,11 @@ for line in sys.stdin:
 	curr_file = file_id
 
 	if( (not bit) or (not score) ):
-		
 		print "incorrect bit score for file",curr_file
 		continue
-
-	# convert to int  
-	bit = int(bit)
-	score = int(score)
-	# assumes each file will occur in chunks 
-	if(not prev_file):
-		# first file
-		# init arr 
-		sketch = [0 for x in xrange(64)]
-		# set first score 
-		if (score > 0 ): 
-			sketch[bit] = '1'
-		else:
-			# when score == 0 or -ve 
-			sketch[bit] = '0'
-		# set prev file 
-		prev_file = curr_file
-	else:
-		if(prev_file == curr_file ):
-			# set sketch bit
-			if (score > 0 ): 
-				sketch[bit] = '1'
-			else:
-				# when score == 0 or -ve 
-				sketch[bit] = '0'
-			# update prev file 
-			prev_file = curr_file
-
-		else:
-		# we have a new file so we emit the prev file
-		#emit stuff
-			sketch_str = ''.join(sketch)
-			s = "%s\t%s,%s"%(str(1),prev_file,sketch_str)
-			print s
-			prev_file = curr_file	
-			# reinitialize the sketch list
-			sketch = [0 for x in xrange(64)]
-			# set first score 
-			if (score > 0 ): 
-				sketch[bit] = '1'
-			else:
-				# when score == 0 or -ve 
-				sketch[bit] = '0'
-
-# Last file
-sketch_str = ''.join(sketch)
-s = "%s\t%s,%s"%(str(1),prev_file,sketch_str)
-print s
-
+	s = "%s\t%s,%s"%(file_id,bit,score)
+	print s
+ 
+	
+	
 	
--- charikar/reducer2.py
+++ charikar/reducer2.py
@@ -1,29 +1,77 @@
 #!/usr/bin/env python
-import sys
+import sys 
+from collections import defaultdict
+# input file \t bit,score
+# the mapper may read the file in any order but we are banking on the fact 
+# that the previous reducer's ouput contains all the entries for a file
 
-# this reducer takes 1 \t file,sketch and output  
-# and calculated the hamming distance of each file
+# this mapper outut the file \t and its sketch 
 
-# reads filenames from a dist cache 
-f = open("file_names.txt")
-text =f.read()
-name_lines = text.split('\n')
-for line in sys.stdin():
+dict_fs = defaultdict(list)
+
+def print_kv(dict_fs):
+
+	for fil  in dict_fs:
+		l = dict_fs[fil]
+		#for i in range(len(l)):
+		sketch_str = ''.join(l)
+		s = "%s\t%s"%(fil,sketch_str)
+		#s = "%s\t%s,%s"%(fil,i,l[i])
+		print s
+
+for line in sys.stdin:
+
 	line = line.strip()
-	if( not line):
+	if (not line):
 		continue
-	fname,sketch = line.split('\t')
-	fname = fname.strip()
-	sketch = sketch.strip()
-	if((not fname) or (not sketch)):
-		print "incorrect input"
-		continue
-	for name in name_lines:
-		name = name.strip()
-		# we dont want to calculate hamming distance for same files
-		if(name == fname):
-			continue 
-		
-	
+	file_id,bit_score = line.split('\t')
+	file_id.strip()
+	#if( (not file_id ) or ( not bit_score) ) :
+	#	print "incorrect input "
+	#	continue
+	bit,score = bit_score.split(',')
 
-	
+	curr_file = file_id
+	#if( (not bit) or (not score) ):
+	#	
+	#	print "incorrect bit score for file",curr_file
+	#	continue
+
+	# convert to int  
+	bit = int(bit)
+	score = int(score)
+	if curr_file in dict_fs:
+
+		if (score > 0 ): 
+			dict_fs[curr_file][bit]= '1'
+		else:
+			# when score == 0 or -ve 
+			dict_fs[curr_file][bit]= '0'
+		#if(curr_file == '2'):
+		#s = "%s\t%s,%s"%(curr_file,bit,dict_fs[curr_file][bit])
+		#print s
+	else:
+		# init 
+		dict_fs[curr_file] = ['$']*64
+		# update score
+		if (score > 0 ): 
+			dict_fs[curr_file][bit]= '1'
+		else:
+			# when score == 0 or -ve 
+			dict_fs[curr_file][bit]= '0'
+		
+		#if(curr_file == '2'):
+		#s = "%s\t%s,%s"%(curr_file,bit,dict_fs[curr_file][bit])
+		#print s
+
+	# assumes each file will occur in chunks
+print_kv(dict_fs)
+"""
+for fil in dict_fs:
+	l = dict_fs[fil]
+	#for i in range(len(l)):
+	sketch_str = ''.join(l)
+	s = "%s\t%s,%s"%(str(1),fil,sketch_str)
+	#s = "%s\t%s,%s"%(fil,i,l[i])
+	print s
+"""	
